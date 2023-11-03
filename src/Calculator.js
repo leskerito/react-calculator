62b162f7d190bfc1bb4e58733b79fa0c8e2eabc8
leskerito <franck.kindia@outlook.fr>
2023-11-03 18:01:10 +0100

    Re-redone the calculation process as Array. In Progress

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,23 +1,49 @@
-import {useState, React} from 'react'
+import React, {useState} from 'react'
 import Buttons from './Buttons';
 
 export default function Calculator() {
     const [currentExpression, setCurrentExpression] = useState("0");
+    const [expElements, setExpElements] = useState([]);
 
-    const evaluate = (expression) => {
-        if(expression === undefined) return '0'
+    const sanitize = (dirtyArray) => {
+        let expArray = [...dirtyArray];
+        for (let exp in expArray){
+            if (isNumber(expArray[exp])){
+                expArray[exp].replace(/[^0[1-9]]/, '');
+            }
+        }
+        return expArray;
+    }
+
+    const evaluate = (dirtyArray) => {
+        if(dirtyArray === undefined) return '0'
+        console.log(dirtyArray)
+        let cleanArray = sanitize(dirtyArray);
+        const expression = cleanArray.join('');
         try {
-            console.log(expression)
-            return eval(expression)
+            return String(eval(expression))
         } catch (error) {
-            alert(error);
+            console.error(error);
             return "0"
         }
     }
 
-    const handleButton = (entry, arithmArray = []) => {
+    const isOperator = (entry) => {
+        return entry.match(/(^\+$)|(^-$)|(^\*$)|(^\/$)/)
+    }
+
+    const isNumber = (entry) => {
+        return entry.match(/[0-9]+/g);
+    }
+
+    const handleButton = (entry) => {
         
+        /*
         switch(entry){
+            case '.':
+                if(currentExpression[currentExpression.length-1] !== '.') setCurrentExpression(prevExpression => prevExpression+entry)
+
+                break;
             case 'E':
                 currentExpression.length > 1 
                 ? setCurrentExpression(prevExpression => prevExpression.slice(0, -1))
@@ -25,24 +51,62 @@ export default function Calculator() {
                 break;
             case 'C':
                 setCurrentExpression('0');
-                arithmArray = [];
                 break;
             case '=':
-                console.log(arithmArray)
+                setCurrentExpression(() => evaluate(currentExpression));
                 break;
             case '/':
             case '*':
             case '-':
             case '+':
-                arithmArray.push(currentExpression)
-                console.log(arithmArray)
+                setExpElements(expElements => expElements.push(currentExpression))
                 setCurrentExpression(prevExpression => prevExpression+entry)
+                break;
+            case '0':
+                currentExpression === '0' ? setCurrentExpression('0') : setCurrentExpression(prevExpression => prevExpression+entry)
                 break;
             default:
                 if(currentExpression === "0")
                     setCurrentExpression(entry)
                 else
                     setCurrentExpression(prevExpression => prevExpression + entry)
+                break;
+        }
+        */
+
+        switch(entry){
+            case 'E':
+                currentExpression.length > 1 ? setCurrentExpression(currentExpression.slice(0, -1)) : setCurrentExpression('0');
+                break;
+            case 'C':
+                setCurrentExpression('0');
+                setExpElements([]);
+                break;
+            case '+':
+            case '-':
+            case '*':
+            case '/':
+                let opExp = [].concat(expElements);
+                opExp.push(currentExpression);
+                setExpElements(opExp);
+                setCurrentExpression(entry);
+                break;
+            case '=':
+                let eqExp = [].concat(expElements);
+                eqExp.push(currentExpression);
+                eqExp = [evaluate(eqExp)]
+                setExpElements(eqExp)
+                setCurrentExpression(eqExp[0]);
+                break;
+            default:
+                if(currentExpression === '0') setCurrentExpression(entry) 
+                else if (isOperator(currentExpression)){
+                    let opExp = [].concat(expElements);
+                    opExp.push(currentExpression);
+                    setExpElements(opExp);
+                    setCurrentExpression(entry);
+                    break;
+                } else setCurrentExpression(prevExpression => prevExpression+entry);
                 break;
         }
     }
